6775b209654a6af857ae8dbcb96ada2421578ad7
Yonathan-Berhanu <yonathan.berhanu17@gmail.com>
2022-10-18 19:03:37 +0300

    users table change

--- laravelauthapi1/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravelauthapi1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('age');
+            $table->string('address')->nullable();
+            $table->string('field_of_study');
+            $table->bigInteger('phone_number');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

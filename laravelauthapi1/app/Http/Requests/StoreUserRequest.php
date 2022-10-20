539c0ef3db4cbc65ebe1fcdfef3512f4c5162246
Amanfikiru <amanuelfikiru49@gmail.com>
2022-10-20 07:13:47 -0700

    edit in auth

---
+++ laravelauthapi1/app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'firstname'=> ['required', 'string', 'max:255'],
+            'lastname'=> ['required', 'string', 'max:255'],
+            'address'=> ['required', 'string', 'max:255'],
+            'age'=> ['integer'],
+            'gender'=> ['required', 'string', 'max:2'],
+            'field_of_study'=> ['required', 'string', 'max:255'],
+            'level_of_study'=> ['required', 'string', 'max:255'],
+            'phone_number'=> ['required', 'integer', 'max:50'],
+            'email'=> ['required', 'string', 'max:255', 'unique:users'],
+            'password'=> ['required', 'confirmed', Rules\password::defaults()]
+        ];
+    }
+}

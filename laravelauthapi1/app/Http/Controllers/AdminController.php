ecea4caa10c9f5642735d8f24588b42ec2d6e0d5
Yonathan-Berhanu <yonathan.berhanu17@gmail.com>
2022-11-02 11:07:33 +0300

    completed admin controller

--- laravelauthapi1/app/Http/Controllers/AdminController.php
+++ laravelauthapi1/app/Http/Controllers/AdminController.php
@@ -1,10 +1,60 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function adminregister(Request $request){
+        $admin = Admin::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        if($admin){
+            return response()->json([
+                $admin, 'status'=>true
+            ]);  
+        }else{
+            return response()->json(['status'=>false]);
+        }
+    }
+
+    public function adminlog(Request $request){
+        $credentials = request(['email', 'password']);
+
+        if(! $token = auth()->guard('admin-api')->attempt($credentials)){
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+            
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+    public function me()
+    {
+        return response()->json(auth()->guard('admin-api')->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->guard('admin-api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 }

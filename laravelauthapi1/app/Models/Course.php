8f9834fbace9b14a95e68d30e464746d4c3898bf
Yonathan-Berhanu <yonathan.berhanu17@gmail.com>
2022-11-03 17:35:28 +0300

    pivot,timestamps

--- laravelauthapi1/app/Models/Course.php
+++ laravelauthapi1/app/Models/Course.php
@@ -29,6 +29,8 @@ class Course extends Model
     }
 
     public function students(){
-        return $this->belongsToMany(Student::class, 'course_student', 'course_id', 'student_id');
+        return $this->belongsToMany(Student::class, 'course_student', 'course_id', 'student_id')
+                    ->withPivot('status', 'module_id')
+                    ->withTimestamps();
     }
 }
